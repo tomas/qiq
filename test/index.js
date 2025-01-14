6dbf91b2bcb82d5e5299a90474d4187f8429a1c3
Tomás Pollak <tomaspollak@gmail.com>
2025-01-14 10:55:34 -0300

    Tests

--- test/index.js
+++ test/index.js
@@ -9,49 +9,49 @@ function mm(template, data, opts) {
 // var mm = require('../dist/qiq.min');
 var should = require('should');
 
-describe('{id}', function(){
-  it('should not work', function(){
+describe('{id}', function() {
+  it('should not work', function() {
     var user = { name: 'tobi' };
     mm('hi {name}.', user).should.equal('hi {name}.');
   })
 })
 
-describe('{{id}}', function(){
-  it('should buffer', function(){
+describe('{{id}}', function() {
+  it('should buffer', function() {
     var user = { name: 'tobi' };
     mm('hi {{name}}.', user).should.equal('hi tobi.');
   })
 
-  it('should escape', function(){
+  it('should escape', function() {
     var user = { name: '<script>' };
     mm('hi {{name}}.', user).should.equal('hi &lt;script&gt;.');
   })
 
-  it('allows spaces in tags', function(){
+  it('allows spaces in tags', function() {
     var user = { name: 'test' };
     mm('hi {{ name }}.', user).should.equal('hi test.');
   })
 
-  it('should support nested props', function(){
+  it('should support nested props', function() {
     var user = { name: { first: 'tobi', last: 'ferret' }};
     mm('hi {{name.first}} {{name.last}}.', user).should.equal('hi tobi ferret.');
   })
 
-  // it('should support bracket props', function(){
+  // it('should support bracket props', function() {
   //   var user = { name: { first: 'tobi', 1: 'ferret' }};
   //   mm('hi {{name["first"]}} {{name[1]}}.', user).should.equal('hi tobi ferret.');
   // })
 
-  it('should escape newlines', function(){
+  it('should escape newlines', function() {
     var user = { name: 'tobi' };
     mm('hi,\nhow are you {{name}}?', user).should.equal('hi,\nhow are you tobi?');
   })
 
-  it('should escape quotes', function(){
+  it('should escape quotes', function() {
     mm('"hey"').should.equal('"hey"');
   })
 
-  it('should allow setting a different delimiter', function(){
+  it('should allow setting a different delimiter', function() {
     // var opts = { delimiter: /\[\[ ?| ?\]\]/ };
     var opts = { delimiters: ['\[\\[', '\]\\]'] };
     var data = { foo: '123', var: 234 }
@@ -73,52 +73,52 @@ describe('{{id}}', function(){
   })
 })
 
-describe('raw filter', function(){
-  it('should be unescaped', function(){
+describe('raw filter', function() {
+  it('should be unescaped', function() {
     var user = { name: '<script>' };
     mm('hi {{name|raw}}.', user).should.equal('hi <script>.');
   })
 })
 
-describe('{{#id}}', function(){
-  it('should pass through when truthy', function(){
+describe('{{#id}}', function() {
+  it('should pass through when truthy', function() {
     var user = { admin: true };
     mm('{{#admin}}yup{{/admin}}', user).should.equal('yup');
   })
 
-  it('should ignore when falsey', function(){
+  it('should ignore when falsey', function() {
     var user = { admin: false };
     mm('admin: {{#admin}}yup{{/admin}}', user).should.equal('admin: ');
   })
 
-  it('should ignore when undefined', function(){
+  it('should ignore when undefined', function() {
     var user = {};
     mm('admin: {{#admin}}yup{{/admin}}', user).should.equal('admin: ');
   })
 
-  it('should support nested tags', function(){
+  it('should support nested tags', function() {
     var user = { admin: true, name: 'tobi' };
     mm('{{#admin}}{{name}} is an admin{{/admin}}', user).should.equal('tobi is an admin');
   })
 
-  it('should support nested conditionals', function(){
+  it('should support nested conditionals', function() {
     var user = { admin: true, authenticated: true };
     mm('{{#admin}}{{#authenticated}}yup{{/}}{{/}}', user).should.equal('yup');
   })
 
-  it('should work with regular HTML', function(){
+  it('should work with regular HTML', function() {
     var user = { admin: true, authenticated: true };
     mm('<div class="login">\n{{#authenticated}}\nlogged in!\n{{/}}\n</div>', user)
       .should.equal('<div class="login">\n\nlogged in!\n\n</div>');
   })
 
-  it('should allow reusing question mark', function(){
+  it('should allow reusing question mark', function() {
     var user = { admin: true, authenticated: true };
     mm('{{admin?}}is admin{{/}} {{admin?}}still admin{{/}}', user)
       .should.equal('is admin still admin');
   })
 
-  it('should support functions that return booleans', function(){
+  it('should support functions that return booleans', function() {
     var obj = {
       bool: function(block) {
         return true
@@ -154,7 +154,7 @@ describe('{{#id}}', function(){
 
   })
 
-  xit('should support functions that receive blocks', function(){
+  xit('should support functions that receive blocks', function() {
     var obj = {
       md: function(str, i) {
         console.log(str, i)
@@ -173,163 +173,163 @@ describe('{{#id}}', function(){
       .should.equal('some <em>markdown</em> ! FOO');
   })
 
-  it('should iterate arrays of objects', function(){
+  it('should iterate arrays of objects', function() {
     var contacts = { contacts: [{ name: 'tobi' }, { name: 'loki' }, { name: 'jane' }] };
     mm('<ul>{{#contacts}}<li>{{.name}}</li>{{_}}bar{{/contacts}}</ul>', contacts)
      .should.equal('<ul><li>tobi</li><li>loki</li><li>jane</li></ul>');
   })
 
-  it('should iterate arrays of elements', function(){
+  it('should iterate arrays of elements', function() {
     var contacts = { numbers: ['one', 'two', 'three'] };
     mm('<ul>{{#numbers}}<li>{{.}}</li>{{_else}}foo{{/}}</ul>', contacts)
      .should.equal('<ul><li>one</li><li>two</li><li>three</li></ul>');
   })
 
-  it('should not iterate nonexisting arrays', function(){
+  it('should not iterate nonexisting arrays', function() {
     var contacts = { numbers: ['one', 'two', 'three'] };
     mm('<ul>{{!numbers}}<li>{{.}}</li>{{_else}}<li>no results</li>{{/}}</ul>', contacts)
      .should.equal('<ul><li>no results</li></ul>');
   })
 
-  it('should not descend into arrays if using question mark', function(){
+  it('should not descend into arrays if using question mark', function() {
     var contacts = { title: 'guys', contacts: [{ name: 'tobi' }, { name: 'loki' }, { name: 'jane' }] };
     mm('<h2>{{ contacts? }}{{ contacts.length }} {{ title }}{{ /contacts? }}</h2>', contacts)
      .should.equal('<h2>3 guys</h2>');
   })
 
-  it('should not descend into arrays if using question mark and not found', function(){
+  it('should not descend into arrays if using question mark and not found', function() {
     var contacts = { title: 'guys', contacts: [{ name: 'tobi' }, { name: 'loki' }, { name: 'jane' }] };
     mm('<h2>{{ others? }}{{ contacts.length }} {{ _ }}no {{ title }}{{ /others? }}</h2>', contacts)
      .should.equal('<h2>no guys</h2>');
   })
 
-  it('should not descend into arrays if using question mark and not found (inverse)', function(){
+  it('should not descend into arrays if using question mark and not found (inverse)', function() {
     var contacts = { title: 'guys', contacts: [{ name: 'tobi' }, { name: 'loki' }, { name: 'jane' }] };
     mm('<h2>{{ !others? }}{{ contacts.length }} {{ _ }}no {{ title }}{{ /others? }}</h2>', contacts)
      .should.equal('<h2>3 </h2>');
   })
 
-  it('should descend into objects if requested and present', function(){
+  it('should descend into objects if requested and present', function() {
     var data = { color: { r: '1', g: '2', b: 3 } };
     mm('{{#color}}{{.r}}-{{.g}}-{{.b}}{{_}}foobar{{/color}}', data).should.equal('1-2-3');
   })
 
-  it('should not descend into objects if not present', function(){
+  it('should not descend into objects if not present', function() {
     var data = { title: 'hello' };
     mm('{{#color}}{{title}}{{_else}}{{title}}{{/color}}', data).should.equal('hello');
   })
 
-  it('should not descend into objects if not present, reversed', function(){
+  it('should not descend into objects if not present, reversed', function() {
     var data = { title: 'hello' };
     mm('{{!color}}{{title}}{{_else}}nope{{/color}}', data).should.equal('hello');
   })
 
-  it('should not descend into objects if present, reversed', function(){
+  it('should not descend into objects if present, reversed', function() {
     var data = { title: 'looks like its there', color: { foo: 'bar' } };
     mm('{{!color}}does not exist{{_else}}{{title}}{{/color}}', data).should.equal('looks like its there');
   })
 
-  it('should not descend into objects if question mark', function(){
+  it('should not descend into objects if question mark', function() {
     var data = { color: { r: '1', g: '2', b: 3 } };
     mm('{{color?}}{{color.r}}-{{color.g}}-{{color.b}}{{_else}}hello{{/color?}}', data).should.equal('1-2-3');
   })
 
-  it('should not descend into objects if question mark, and not present', function(){
+  it('should not descend into objects if question mark, and not present', function() {
     var data = { color: { r: '1', g: '2', b: 3 } };
     mm('{{missing?}}missing{{_else}}{{color.r}}-{{color.g}}-{{color.b}}{{/missing?}}', data).should.equal('1-2-3');
   })
 
 })
 
-describe('{{!id}}', function(){
+describe('{{!id}}', function() {
 
-  it('should ignore when truthy', function(){
+  it('should ignore when truthy', function() {
     var user = { admin: true };
     mm('{{!admin}}yup{{/admin}}', user).should.equal('');
   })
 
-  it('should pass through when falsey', function(){
+  it('should pass through when falsey', function() {
     var user = { admin: false };
     mm('admin: {{!admin}}nope{{/admin}}', user).should.equal('admin: nope');
   })
 
-  it('should support nested tags', function(){
+  it('should support nested tags', function() {
     var user = { admin: false, name: 'tobi' };
     mm('{{!admin}}{{name}} is not an admin{{/admin}}', user).should.equal('tobi is not an admin');
   })
 
-  it('should support nested conditionals', function(){
+  it('should support nested conditionals', function() {
     var user = { admin: false, authenticated: false };
     mm('{{!admin}}{{!authenticated}}nope{{/}}{{/}}', user).should.equal('nope');
   })
 
-  it('should consider empty arrays falsy', function(){
+  it('should consider empty arrays falsy', function() {
     var users = { users: [] };
     mm('users exist: {{!users}}nope{{/users}}', users)
      .should.equal('users exist: nope');
   })
 
-  it('should consider empty arrays falsy with else', function(){
+  it('should consider empty arrays falsy with else', function() {
     var users = { users: [] };
     mm('users exist: {{users?}}yes{{_else}}nope{{/users?}}', users)
      .should.equal('users exist: nope');
   })
 
-  it('should process populated arrays', function(){
+  it('should process populated arrays', function() {
     var users = { users: [ 'tobi' ] };
     mm('users exist: {{#users}}yep, {{.}}{{/users}}{{!users}}nope{{/users}}', users)
      .should.equal('users exist: yep, tobi');
   })
 
-  it('should include indexes', function(){
+  it('should include indexes', function() {
     var users = { users: [ 'tom', 'mot' ] };
     mm('users:{{#users}} {{$idx}} -> {{.}}{{/users}}', users)
      .should.equal('users: 0 -> tom 1 -> mot');
   })
 
-  it('should honor ifelse', function(){
+  it('should honor ifelse', function() {
     var data = { fails: false };
     mm('fails? {{fails?}}yep{{_else}}nope{{/fails}}', data)
      .should.equal('fails? nope');
   })
 
-  it('should honor ifelse with "else" keyword too', function(){
+  it('should honor ifelse with "else" keyword too', function() {
     var data = { fails: false };
     mm('fails? {{#fails}}yep{{_else}}nope{{/fails}}', data)
      .should.equal('fails? nope');
   })
 
-  it('should honor ifelse (inverted)', function(){
+  it('should honor ifelse (inverted)', function() {
     var data = { works: true };
     mm('fails? {{!works}}yep{{_else}}nope{{/works}}', data)
      .should.equal('fails? nope');
   })
 
-  it('ifelse works with single char', function(){
+  it('ifelse works with single char', function() {
     var data = { works: true };
     mm('fails? {{!works}}yep{{_}}nope{{/}}', data)
      .should.equal('fails? nope');
   })
 
-  it('should supported nested ifelses', function(){
+  it('should supported nested ifelses', function() {
     var data = { fails: false, hot: false };
     mm('fails? {{fails?}}yep{{_else}}nope, {{hot?}}not cool{{_else}}cool!{{/}}{{/}}', data)
      .should.equal('fails? nope, cool!');
   })
 
-  it('should support nested ifelses', function(){
+  it('should support nested ifelses', function() {
     var data = { fails: false, hot: true };
     mm('fails? {{fails?}}yep{{_else}}nope, {{!hot}}not cool{{_else}}cool!{{/}}{{/}}', data)
      .should.equal('fails? nope, cool!');
   })
 
-  it('can do nested question mark and then descend block', function(){
+  it('can do nested question mark and then descend block', function() {
     var data = { products: [ { name: 'one' }, { name: 'two' } ] };
     mm('{{products?}}Products: {{products.length}} --> {{#products}}{{.name}} {{/products}}{{/products?}}', data)
      .should.equal('Products: 2 --> one two ');
   })
 
-  it('can do triple nested blocks', function(){
+  it('can do triple nested blocks', function() {
     var data = {
       menu: 'Catalog',
       links: [
@@ -346,7 +346,7 @@ describe('{{!id}}', function(){
 })
 
 // describe('{{ _else @if }}', function() {
-//   it('should work', function(){
+//   it('should work', function() {
 //     var data = { foo: false, bar: true };
 //     mm('{{ foo? }}foo{{ _else @if bar == 123 }}bar!{{ / }}', data)
 //      .should.equal('bar!');
@@ -355,7 +355,7 @@ describe('{{!id}}', function(){
 
 describe('{{bool?}}', function() {
 
-  it('should work as expected', function(){
+  it('should work as expected', function() {
     var data = {
       trueThing: true,
       falseThing: false
@@ -419,14 +419,31 @@ describe('deep objects', function() {
     mm('{{nested?}}{{#nested}}{{.prop?}}awesome{{_else}}not so awesome{{/.prop?}}{{/nested}}{{/nested?}}', obj).should.equal('awesome');
   })
 
-  it('should traverse objects with dots', function(){
+  it('should traverse objects with dots', function() {
     var data = { my: { color: { r: '1', g: '2', b: 3 } } } ;
     mm('{{my.color.r}}-{{my.color.g}}-{{my.color.b}}', data).should.equal('1-2-3');
   })
 
-  it('should descend into objects with dots', function(){
+  it('should descend into objects with dots', function() {
     var data = { my: { name: 'superman', color: { r: '1', g: '2', b: 3 } } };
     mm('{{#my.color}}{{.r}}-{{.g}}-{{.b}} -- {{my.name}}{{/my.color}}', data).should.equal('1-2-3 -- superman');
+  })
+
+  it('works with arrays within arrays', function() {
+    var data = {
+      products: [
+        { name: 'one', variants: [{name: 'A'}, {name: 'B'}] },
+        { name: 'two', variants: [{name: 'C'}, {name: 'D'}] }
+      ]
+    }
+
+    mm(`
+      {{ #products }}
+        {{ #.variants }}
+          {{ ..name }} - {{ .name }}/
+        {{ /.variants }}
+      {{ /products }}
+    `, data, { htmltrim: true }).should.equal('one - A/one - B/two - C/two - D/');
   })
 
   xit('forbids rewriting condition', function() {
@@ -447,9 +464,9 @@ describe('deep objects', function() {
 
 })
 
-describe('helper/filter functions', function(){
+describe('helper/filter functions', function() {
 
-  it('works with strings', function(){
+  it('works with strings', function() {
     var data = { number: 123, yup: "yup", admin: true };
     var filters = {
       my_helper: function(str, data) {
@@ -460,7 +477,7 @@ describe('helper/filter functions', function(){
     mm("{{admin?}}{{ 'yup' | my_helper }}{{/admin?}}", data, { filters: filters }).should.equal('YUP123');
   })
 
-  it('works with props', function(){
+  it('works with props', function() {
     var data = { number: 123, foo: "yuppie", admin: true };
     var filters = {
       my_helper: function(str, data) {
@@ -471,7 +488,7 @@ describe('helper/filter functions', function(){
     mm("{{admin?}}{{ foo | my_helper }}{{/admin?}}", data, { filters: filters }).should.equal('YUPPIE123');
   })
 
-  it('works within objects', function(){
+  it('works within objects', function() {
     var data = { user: { first_name: "Tom", last_name: "Po" } };
     data.user.full_name = function(user, data) {
       return [user.first_name, user.last_name].join(' ')
@@ -479,7 +496,7 @@ describe('helper/filter functions', function(){
     mm('{{#user}}{{ .full_name }}{{/user}}', data).should.equal('Tom Po');
   })
 
-  it('works with array items', function(){
+  it('works with array items', function() {
     var data = { foo: 123, users: [{ name: 'One'}, {name: 'Two'}, { name: 'Three'}] };
     data.upcase = function(user, data) {
       // var user = data._it;
